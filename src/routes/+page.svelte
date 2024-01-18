1b34d7959cf4104e5351b40a8312f1b963e1188d
May <40745122+SomewhatMay@users.noreply.github.com>
2024-01-17 20:54:35 -0500

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Kocab!</h1>
+<p>Currently under development, please check us out at <a href="https://github.com/Arctos6135/Kocab">the Kocab github</a>.</p>
