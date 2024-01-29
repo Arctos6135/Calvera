d800a4a330a88303d0bc72e93012d9dc63edc871
SomewhatMay <40745122+SomewhatMay@users.noreply.github.com>
2024-01-28 22:31:47 -0500

    tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
 <h1>Welcome to Kocab!</h1>
 <p>Currently under development, please check us out at <a href="https://github.com/Arctos6135/Kocab">the Kocab github</a>.</p>
