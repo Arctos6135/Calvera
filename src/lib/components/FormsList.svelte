22d448cba52bde2445507f7c681e6bfa205a58a9
unknown <micah.hewko@student.tdsb.on.ca>
2024-01-29 22:44:46 -0500

    Added the FormsList.svelte component.

--- src/lib/components/FormsList.svelte
+++ src/lib/components/FormsList.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-    import { goto } from "$app/navigation"
+    import { goto } from "$app/navigation";
 
     import { submitResponse, deleteResponse } from "$lib/actions";
+
+    import { activeResponses, errors, response } from "$lib/store";
+
+    const setResponse = (id: number) => {
+        $response = id;
+        goto("/form");
+    };
+
+    
 </script>
